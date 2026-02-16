99a58401244795589bb2e39463e6188bee44e7b1
Wesley <wesley9063-700@outlook.com>
2026-02-16 17:16:21 -0300

    Adicionando novos imports ao arquivo views.py

--- escola3/cursos/views.py
+++ escola3/cursos/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
+from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
@@ -13,6 +17,11 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     set
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id = self.kwargs.get('curso_pk'))
+        return self.queryset.all()
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()

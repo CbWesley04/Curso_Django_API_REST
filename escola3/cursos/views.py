9a19578ad2a12c7286896eb1177011258333803c
Wesley <wesley9063-700@outlook.com>
2026-02-16 18:09:53 -0300

    Criando 2ª Versão do projeto, modificando o arquivo views.py

--- escola3/cursos/views.py
+++ escola3/cursos/views.py
@@ -6,6 +6,9 @@ from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+
+# ============================================= API V1 =============================================
+
 class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
@@ -31,3 +34,16 @@ class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
         if self.kwargs.get('curso_pk'):
             return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'), pk=self.kwargs.get('avaliacao_pk'))
         return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+    
+
+# ============================================= API V2 =============================================
+
+class CursoViewset(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+    @action(detail=True, methods=['get'])
+    def avaliacoes(self, request, pk=None):
+        curso = self.get_object()
+        serializer = AvaliacaoSerializer(curso.avaliacoes, many=True)
+        return Response(serializer.data)

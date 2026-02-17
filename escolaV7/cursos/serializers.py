a09d75e530e5fb54fb00fb72ce4addbce0529743
Wesley <wesley9063-700@outlook.com>
2026-02-17 20:48:26 -0300

    Adicionando Arquivos de Teste para Testes Automatizados

--- escolaV7/cursos/serializers.py
+++ escolaV7/cursos/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Curso, Avaliacao
+from django.db.models import Avg
 
 class AvaliacaoSerializer(serializers.ModelSerializer):
 
@@ -10,6 +11,11 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
         model = Avaliacao
         fields = ('id', 'curso', 'nome', 'email', 'comentario', 'avaliacao', 'criacao', 'ativo')
 
+    def validate_avaliacao(self, value):
+        if value in range(1, 6):
+            return value
+        raise serializers.ValidationError('A avaliação deve ser entre 1 e 5.')
+
 class CursoSerializer(serializers.ModelSerializer):
 
     # 1. Nested Relationship:
@@ -18,6 +24,8 @@ class CursoSerializer(serializers.ModelSerializer):
     # 3. Primary Key Related Field:
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
+    media_avaliacoes = serializers.SerializerMethodField()
+
     class Meta:
         model = Curso
         fields = ('id', 
@@ -25,5 +33,12 @@ class CursoSerializer(serializers.ModelSerializer):
                 'url', 
                 'criacao', 
                 'ativo', 
-                'avaliacoes', 
+                'avaliacoes',
+                'media_avaliacoes', 
                 )
+    
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+        if media is None:
+            return 0
+        return round(media*2) / 2

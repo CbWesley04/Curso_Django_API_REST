250280f68d9714c6e4b07d3b07abace04af696af
Wesley <wesley9063-700@outlook.com>
2026-02-16 21:37:53 -0300

    Testando o relacionamento entre models

---
+++ escola4/cursos/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Curso, Avaliacao
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = Avaliacao
+        fields = ('id', 'curso', 'nome', 'email', 'comentario', 'avaliacao', 'criacao', 'ativo')
+
+class CursoSerializer(serializers.ModelSerializer):
+
+    # 1. Nested Relationship:
+    # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+    # 2. Hyperlinked Related Field:
+    # 3. Primary Key Related Field:
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Curso
+        fields = ('id', 'titulo', 'url', 'criacao', 'ativo', 'avaliacoes', )

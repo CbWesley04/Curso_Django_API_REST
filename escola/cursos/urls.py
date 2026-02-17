62e7219773d065282a571a16f4e495bd194023b9
Wesley <wesley9063-700@outlook.com>
2026-02-17 19:51:20 -0300

    Versionando as Pastas do Projeto

--- escola/cursos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CursosAPIView, AvaliacoesAPIView
-
-urlpatterns = [
-    path('cursos/', CursosAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes')
-]
